f6b9f4e5483ba7dfbac7b578cdc32c45380370fa
Junior <juniorgarciadiaz@outlook.com>
2022-03-03 00:04:44 +0000

    Introducción a la primera clase

--- Python_Curse/Class/01_Helloworld.py
+++ Python_Curse/Class/01_Helloworld.py
@@ -2,3 +2,13 @@
 # En esta ocasión vamos a aprender a realizar nuestro primer programa en Python
 
 print("Hello World!!!")
+
+# Gracias a print podemos mandar a consola todo lo que nosotros queramos, desde simpes mensajes a datos almacenados en una variable
+
+# Una variable es una caja vacía a la que le podemos introducir información en python se identifican de la siguiente manera
+
+saludo = "Hola"
+
+# saludo es el nombre de la variable y lo que viene después del igual es su contenido, se pueden almacenar multitud de cosas dentro de ellas pudiendo usarse en otros lugares gracias a su nombre
+
+print ( saludo + " Junior")
---
+++ Python_Curse/Exercises/Datos simples/Ejercicio_1.py
@@ -0,0 +1,30 @@
+# Crear un programa que muestre por pantalla un Hello World
+
+print("Hello World")
+
+# Crear un programa que almacene el string en una variable y luego lo muestre por consola
+
+Saludo = "Hello World"
+
+print (Saludo)
+
+# Introducir el nombre de Juan en una variable e imprimir por pantalla su nombre junto al saludo
+
+nombre = 'Juan'
+
+print("Hola" + nombre)
+
+# Introducir el color Violeta junto al mensaje me gusta el color e imprimirlo por pantalla
+
+color = 'violeta'
+
+mensaje = 'me gusta el color '
+
+print("Hola " + mensaje + color)
+
+# Crear un mensaje en cosola con tu nombre y tus aficiones todo ello a través del uso de variables
+
+nombre, afición = 'Junior', 'Programar y jugar juegos'
+
+print("Hola!!, mi nombre es " + nombre + " y mis aficiones son: " + afición)
+
